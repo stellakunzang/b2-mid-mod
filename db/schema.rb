5c26563a2328de785d96351c17b804094506e9ac
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-22 09:39:35 -0600

    Add studio foreign key to movies table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200522152531) do
+ActiveRecord::Schema.define(version: 20200522153442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20200522152531) do
     t.string "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "studio_id"
+    t.index ["studio_id"], name: "index_movies_on_studio_id"
   end
 
   create_table "studios", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 20200522152531) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "movies", "studios"
 end
