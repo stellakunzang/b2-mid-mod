41571f0c7bafac9e61bb4d0e399126b4f732f80b
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-22 10:53:56 -0600

    Create model method to order actors by age

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,9 @@ class Movie < ApplicationRecord
   validates_presence_of :title, :year, :genre
   belongs_to :studio
   has_many :movie_actors
-  has_many :actors, through: :movie_actors 
+  has_many :actors, through: :movie_actors
+
+  def actors_by_age
+    actors.order(:age)
+  end
 end
