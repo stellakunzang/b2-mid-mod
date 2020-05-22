fa6491a02ce3ed97371b136ac31f336d89ff0284
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-22 09:50:15 -0600

    Remove s from class name in spec test

--- spec/models/movie_actors_spec.rb
+++ spec/models/movie_actors_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe MovieActors do
+RSpec.describe MovieActor do
   describe "relationships" do
     it {should belong_to :movie}
     it {should belong_to :actor}
