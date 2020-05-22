128a3e50e52eae954d8767b6bd90185d67a0752d
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-22 10:36:40 -0600

    Add actors and movie actors to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,9 @@ studio2 = Studio.create(name: "Another Movie Studio", location: "Mars")
 
 movie1 = Movie.create(title: "A Movie", year: "1999", genre: "Spooky", studio_id: "#{studio1.id}")
 movie2 = Movie.create(title: "Movie: Sequel", year: "2002", genre: "Spooky", studio_id: "#{studio1.id}")
+
+actor1 = Actor.create(name: "John Prawn", age: 25)
+actor2 = Actor.create(name: "Jane Grain", age: 40)
+
+MovieActor.create(movie_id: "#{movie1.id}", actor_id: "#{actor1.id}")
+MovieActor.create(movie_id: "#{movie1.id}", actor_id: "#{actor2.id}")
