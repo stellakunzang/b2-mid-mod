dec944208feede8a7af5ce197375961f0dfaa3d3
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-22 09:32:31 -0600

    Add movies to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200522152021) do
+ActiveRecord::Schema.define(version: 20200522152531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "year"
+    t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "studios", force: :cascade do |t|
     t.string "name"
