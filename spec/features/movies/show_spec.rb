c282ab7768f399376039132b9b1a2ac1ca7fd3f5
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-22 11:04:39 -0600

    Add test for form on movie show page to add actor

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -46,10 +46,35 @@ RSpec.describe "movie show page" do
 
     expect(page).to have_content("Average Age = 25")
   end
+
+  it "should display a form to add an existing actor to the movie" do
+    studio1 = Studio.create(name: "The Movie Studio", location: "The Moon")
+    movie1 = Movie.create(title: "A Movie", year: "1999", genre: "Spooky", studio_id: "#{studio1.id}")
+
+    actor1 = Actor.create(name: "John Prawn", age: 25)
+    actor2 = Actor.create(name: "Jane Grain", age: 40)
+    actor3 = Actor.create(name: "Lou Glue", age: 10)
+
+    MovieActor.create(movie_id: "#{movie1.id}", actor_id: "#{actor1.id}")
+    MovieActor.create(movie_id: "#{movie1.id}", actor_id: "#{actor2.id}")
+
+    visit "/movies/#{movie1.id}"
+
+    expect(page).to have_no_content(actor3.name)
+
+    fill_in :name, with: "#{actor3.name}"
+
+    expect(current_path).to eq("/movies/#{movie1.id}")
+    expect(page).to have_content(actor3.name)
+  end
 end
-# Story 2
+
+
+# Story 3
 # As a visitor,
-# When I visit a movie's show page.
-# I see the movie's title, creation year, and genre,
-# and a list of all its actors from youngest to oldest.
-# And I see the average age of all of the movie's actors
+# When I visit a movie show page,
+# I see a form for an actors name
+# and when I fill in the form with an existing actor's name
+# I am redirected back to that movie's show page
+# And I see the actor's name listed
+# (This should not break story 3, You do not have to test for a sad path)
