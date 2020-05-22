b7cb713e42ff7e2ed69069f6acbf99ee334bb8db
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-22 10:58:57 -0600

    Add test for actors average age to movie model spec

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -22,5 +22,20 @@ RSpec.describe Movie do
 
       expect(movie1.actors_by_age).to eq([actor3, actor1, actor2])
     end
+
+    it "#actors_average_age" do
+      studio1 = Studio.create(name: "The Movie Studio", location: "The Moon")
+      movie1 = Movie.create(title: "A Movie", year: "1999", genre: "Spooky", studio_id: "#{studio1.id}")
+
+      actor1 = Actor.create(name: "John Prawn", age: 25)
+      actor2 = Actor.create(name: "Jane Grain", age: 40)
+      actor3 = Actor.create(name: "Lou Glue", age: 10)
+
+      MovieActor.create(movie_id: "#{movie1.id}", actor_id: "#{actor1.id}")
+      MovieActor.create(movie_id: "#{movie1.id}", actor_id: "#{actor2.id}")
+      MovieActor.create(movie_id: "#{movie1.id}", actor_id: "#{actor3.id}")
+
+      expect(movie1.actors_average_age).to eq(25)
+    end
   end
 end
