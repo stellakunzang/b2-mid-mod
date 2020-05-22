3d9b30cdbf76bbe8674229cb5f0bc7941daa95ce
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-22 09:55:46 -0600

    Add relationships to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
   validates_presence_of :title, :year, :genre
-  belongs_to :studio 
+  belongs_to :studio
+  has_many :movie_actors
+  has_many :actors, through: :movie_actors 
 end
