9182da87f5c9b0589635a4e634aec794a7ffbfab
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-22 10:41:04 -0600

    Add test for order of actor names on movie show page

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -13,6 +13,21 @@ RSpec.describe "movie show page" do
   end
 
   it "should list all actors from youngest to oldest" do
+    studio1 = Studio.create(name: "The Movie Studio", location: "The Moon")
+    movie1 = Movie.create(title: "A Movie", year: "1999", genre: "Spooky", studio_id: "#{studio1.id}")
+
+    actor1 = Actor.create(name: "John Prawn", age: 25)
+    actor2 = Actor.create(name: "Jane Grain", age: 40)
+    actor3 = Actor.create(name: "Lou Glue", age: 10)
+
+    MovieActor.create(movie_id: "#{movie1.id}", actor_id: "#{actor1.id}")
+    MovieActor.create(movie_id: "#{movie1.id}", actor_id: "#{actor2.id}")
+    MovieActor.create(movie_id: "#{movie1.id}", actor_id: "#{actor3.id}")
+
+    visit "/movies/#{movie1.id}"
+
+    expect(actor1.name).to appear_before(actor2.name)
+    expect(actor3.name).to appear_before(actor1.name)
   end
 
   it "should display average age of all the actors" do
