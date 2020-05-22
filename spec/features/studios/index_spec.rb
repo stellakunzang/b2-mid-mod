a49879b6efa3d2403fccf71d0c7b3de96a95963e
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-22 10:17:25 -0600

    Add test for movie titles displayed on studio index page

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -13,6 +13,22 @@ RSpec.describe "studio index page" do
 
   it "should display the names of movies underneath each studio" do
     studio = Studio.create(name: "The Movie Studio", location: "The Moon")
+    studio2 = Studio.create(name: "Another Movie Studio", location: "Mars")
+    movie1 = Movie.create(title: "A Movie", year: "1999", genre: "Spooky", studio_id: "#{studio.id}")
+    movie2 = Movie.create(title: "Movie: Sequel", year: "2002", genre: "Spooky", studio_id: "#{studio.id}")
+
+    visit "/studios"
+
+    within ".studio-#{studio.id}" do
+      expect(page).to have_content(movie1.title)
+      expect(page).to have_content(movie2.title)
+    end
+
+    within ".studio-#{studio2.id}" do
+      expect(page).to have_no_content(movie1.title)
+      expect(page).to have_no_content(movie2.title)
+    end
+
   end
 end
 
