9d6824ed0da07e98503f7f9d2f3530b621d56e69
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-22 10:05:15 -0600

    Add route for studio index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+studio1 = Studio.create(name: "The Movie Studio", location: "The Moon")
+studio2 = Studio.create(name: "Another Movie Studio", location: "Mars")
